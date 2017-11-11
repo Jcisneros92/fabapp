10078770e77fff91b77098da182bbe28d862e486
Jcisneros92 <Jcisneros92@gmail.com>
2017-11-11 00:41:45 -0600

    commit from 11/11
    • Added ability to choose date
    • Added ability to choose amount of days before chosen date
    • Added ability to choose color from drop down menu
    • Added color estimation results from selected date

--- inventory_edit/index.php
+++ inventory_edit/index.php
@@ -6,26 +6,31 @@
 include_once ($_SERVER['DOCUMENT_ROOT'].'/pages/header.php');
 if (!$staff || $staff->getRoleID() < 7){
     //Not Authorized to see this Page
-    header('Location: index.php');
+    header('Location: /index.php');
 }
+
+if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["maybe_don't_use_generic_names_for_btns"]) ){
+	//  At this point you need to decide where you want to scrub the user input
+	//	You must be aware that they user could have malicious intent, see SQL injection
+	//	Here I can already assume that this value must be an integar and it must already
+	//	exist in the device_group table.  I will call a static function to verify this.
+	$quantity = filter_input(INPUT_POST,'quantity');
+	$selection = filter_input(INPUT_POST,"select_value_comes_from_option's_value");
+
+	//if someone types in a string what will happen to your code?
+	if($quantity < 100)
+	{
+		echo "<script> alert('Qty = $quantity, Material ID = $selection')</script>";
+	}
+
+	else
+	{
+		header("Location:/inventory_edit/index.php");
+	}
+}
+	
 ?>
 
-
-
-<!-- ##### connection and list array creation #####-->
-<?php
-$arr = array();
-
-$conn = new mysqli('localhost', 'root', '', 'fabapp-v0.9');
-$sql = "SELECT m_name FROM  materials WHERE m_parent = 1";
-$result = $conn->query($sql);
-
-while($row = $result->fetch_assoc()) {
-  $name = $row["m_name"];
-  array_push($arr, $name);
-}
-?>
-<!-- ##### end of connection and list array creation #####-->
 
 
 <html>
@@ -41,6 +46,7 @@ function validate() {
 		alert('please enter a quantity');
 		return false;
 	}
+	
 }
 </script>
 
@@ -51,37 +57,53 @@ function validate() {
 <head>
 </head>
 <body>
+<title><?php echo $sv['site_name'];?> Add Inventory</title>
+<div id="page-wrapper">
+    <div class="row">
+        <div class="col-md-12">
+			<h1 class="page-header">Page Name</h1>
+        </div>
+        <!-- /.col-md-12 -->
+    </div>
+    <!-- /.row -->
+    <div class="row">
+        <div class="col-md-8">
+		<div class="panel panel-default">
+                <div class="panel-heading">
+                    <i class="fa fa-ticket fa-fw"></i> refer to the base.php file that I sent you.
+                </div>
+                <div class="panel-body">
+					<form method="post" action="" onsubmit="return validate()" autocomplete='off'>
 
-</form>
-<div style= "text-align: center">
-<form method="get" action="handler.php">
+					<!-- ##### color name and list #####-->
+					<label>Color: </label>
+					<select name = "select_value_comes_from_option's_value">
+						<option hidden disabled selected>Select</option>
+						<?php
+						$arr = array();
+						//you need to request both the unique identifier and the description
+						$sql = "SELECT `m_id`, `m_name` FROM  `materials` WHERE `m_parent` = 1";
+						$result = $mysqli->query($sql);
 
-<!-- ##### color name and list #####-->
-<label>Color: </label>
-<?php
-echo '<select name = "color">';
-for($i = 0; $i < count($arr); $i++)
-{
-	echo '<option>'.$arr[$i].'</option>';
-}
-echo '</select>';
-?>
-<!-- ##### end of color name and list #####-->
+						while($row = $result->fetch_assoc()) {
+							echo "<option value=$row[m_id]>$row[m_name]</option>";
+						}
+						?>
+					</select>
+					<!-- ##### end of color name and list #####-->
+					<label>Quantity: </label>
+					<input type="text" id="quan" name="quantity" />
+					<br/>
 
-
-
-<label>Quantity: </label>
-<input type="text" id="quan" name="quantity" />
-<br/>
-
-<input type="submit" name="submit" id="submit" value="submit here">
-
-
-</form>
-
-
-
-
+					<input type="submit" name="maybe_don't_use_generic_names_for_btns" id="submit" value="submit here">
+					</form>
+				</div>
+            </div>
+        </div>
+	</div>
+    <!-- /.row -->
+</div>
+<!-- /#page-wrapper -->
 </body>
 </html>
 
